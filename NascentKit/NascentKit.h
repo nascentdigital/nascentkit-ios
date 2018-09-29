b7f906d6fde07e53826c5d30d6178c39a2f66d8a
Simeon de Dios <simeon.dedios@gmail.com>
2018-09-29 19:18:28 -0400

    Initial Commit

---
+++ NascentKit/NascentKit.h
@@ -0,0 +1,19 @@
+//
+//  NascentKit.h
+//  NascentKit
+//
+//  Created by Simeon de Dios on 2018-09-29.
+//  Copyright © 2018 Nascent Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NascentKit.
+FOUNDATION_EXPORT double NascentKitVersionNumber;
+
+//! Project version string for NascentKit.
+FOUNDATION_EXPORT const unsigned char NascentKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NascentKit/PublicHeader.h>
+
+
